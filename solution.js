6d170ff9de5b9927c1c1da3f3b2aa7c8dc7711ed
Maria Vasquez <mariadelmarvze@gmail.com>
2022-03-21 21:39:56 -0700

    7 done, need to finish 6 and fix 4

--- solution.js
+++ solution.js
@@ -94,9 +94,56 @@ function mouseLeave(){
 }
 
 //////////////Solution 6//////////////////////
+function selectNewsHeadline(){
+    let newsContent = ["On Sunday, the Saskatchewan Health Authority informed Prairie Spirit School Division of a case of COVID-19 in an individual at Warman Community Middle School. The school division says those affected have been notified.", "Hamilton Public Health reported 74 new COVID-19 cases on the weekend and five more deaths involving four long-term care (LTC) homes plus one in the community.", "The spirit of giving is coming a little earlier this year as volunteers gather at Place Émilie-Gamelin in Montreal on Sunday to distribute food to the homeless.", "RCMP in Lake Louise are warning of hefty fines if residents do not adhere mandatory COVID-19 rules. In a statement, the detachment said anyone who is found breaking a mandatory measure implemented by the province can face a fine of up to $1,000.", "Alberta Premier Jason Kenney calls an attempt by the Michigan government to shutdown the Enbridge Line 5 pipeline very concerning and a continued effort to landlock Canadian energy."];
+    
+    let headlines = document.getElementsByName("headline")[0].selectedIndex;
 
+    let displayNews = document.getElementById("news");
+    
+    for (let i = 0; i < newsContent.length; i++){
+        if (newsContent[i] === headlines[i]){
+            displayNews.innerText = newsContent[i];
+        }
+    }
+}
 
 //////////////Solution 7//////////////////////
+function CalculateRoots(){
+    let A = document.getElementById("a").value;
+    let B = document.getElementById("b").value;
+    let C = document.getElementById("c").value;
+    
+    let printResult = document.getElementById("soln");
 
-
-
+    // Check if values are valid: 
+    if (A < -99 || A > 99){
+        document.getElementById("a").style.borderColor = "red";
+    } else if (A === 0){
+        console.error("Value Can't be 0");
+    }
+    else if (B < -99 || B > 99){
+        document.getElementById("b").style.borderColor = "red"
+    }
+    else if (C < -99 || C > 99){
+        document.getElementById("c").style.borderColor = "red"
+    } 
+    else {
+        //Calculation 
+        let b_squared = B * B;
+        let secondSection = 4 * A * C;
+        let sqr_root = Math.sqrt(b_squared - secondSection);
+        let denominator = 2 * A; 
+        let root1 = (-B + sqr_root) / denominator;
+        let root2 = (-B - sqr_root) / denominator;
+    
+        if(b_squared < secondSection){
+            printResult.innerHTML = "Solution: x's roots are imaginary";
+        } else if (b_squared === secondSection){
+            printResult.innerHTML = "Solution: x = single-root";
+        } else {
+            printResult.innerHTML = `Solution: x= ${root1.toFixed(3)}, x= ${root2.toFixed(3)}`;
+        }
+    }   
+}
+//Ex: -5, 5, 5 = -0.618 and 1.618
